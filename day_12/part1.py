77dfe0c08a1f1aeae94edd43a827594a63983d97
Peter Magdík <petermagdik@gmail.com>
2026-08-24 20:03:19 +0200

    day_12 part 1 - cheesed algo

--- day_12/part1.py
+++ day_12/part1.py
@@ -23,6 +23,33 @@ with open('day_12/input.txt') as f:
             width, height = map(int, dimensions.split('x'))
             number_of_shapes = tuple(map(int, number_of_shapes.split(' ')))
             christmas_tree_regions.append((width, height, number_of_shapes))
+"""
+# converting shapes to bitmap
+# 3x3 is converted to 9 bit long int from left to right, top to bottom
+bitmap_shapes: dict[int, int] = {}
+# bitmap is in reverse
+for shape_id, lines in shapes.items():
+    bitmap = 0
+    for y, line in enumerate(lines):
+        for x, char in enumerate(line):
+            if char == '#':
+                bitmap |= 1 << (y * 3 + x)
+    bitmap_shapes[shape_id] = bitmap
+    print(f"Shape {shape_id} -> {bitmap:09b}")
 
-print(shapes)
-print(christmas_tree_regions)
+"""
+
+# this should not worked, but it did ...
+shapes_space_count: dict[int, int] = {}
+for shape_id, lines in shapes.items():
+    space_count = sum(line.count('#') for line in lines)
+    shapes_space_count[shape_id] = space_count
+
+result = 0
+for width, height, number_of_shapes in christmas_tree_regions:
+    total_space = width * height
+    total_shapes_space = sum(shapes_space_count[shape_id] * count for shape_id, count in zip(shapes_space_count.keys(), number_of_shapes))
+    if total_shapes_space <= total_space:
+        result += 1
+
+print(result)
